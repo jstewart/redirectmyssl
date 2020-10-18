0dfa7e5baae43e031764ac541f50fa466825c590
Jason Stewart <jstewart@fusionary.com>
2020-10-18 09:25:24 -0400

    spec deprecation fixes and cleanups

--- spec/helpers/redirects_helper_spec.rb
+++ spec/helpers/redirects_helper_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe RedirectsHelper, type: :helper do
   context "#with_built_host" do
     it "builds a new record for nested form" do
       redirect = build(:redirect)
-      redirect.hosts.length.should eq(0)
+      expect(redirect.hosts.length).to eq(0)
       helper.with_built_host(redirect)
-      redirect.hosts.length.should eq(1)
+      expect(redirect.hosts.length).to eq(1)
     end
   end
 end
